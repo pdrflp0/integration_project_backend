287bbcb98c525f7f42646f3be1442a93ffe588ee
pdrflp0 <pedrofelipewk@gmail.com>
2024-02-22 20:24:06 -0300

    ### Added    - Category lines

---
+++ src/main/java/br/eletra/Main.java
@@ -0,0 +1,7 @@
+package br.eletra;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/br/eletra/categories/Lines.java
@@ -0,0 +1,14 @@
+package br.eletra.categories;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class Lines {
+    public ObservableList<String> lines = FXCollections.observableArrayList(
+            "Cronos",
+            "Ares"
+    );
+}
